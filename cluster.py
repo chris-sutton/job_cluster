817130b6cba9727bcb93596c5bc92402472cba97
Christopher Sutton <ctsutton1234@gmail.com>
2020-10-16 09:41:07 -0500

    backup

--- cluster.py
+++ cluster.py
@@ -7,8 +7,15 @@ HOSTS = f.read().split('\n')
 
 f = open("CMDS", "r")
 CMDS = f.read().split('\n')
-output = []
-for cmd in CMDS:
-    output.append(subprocess.run(cmd, shell=True, capture_output=True))
 
-print(output)
+# while len(CMDS) != 0:
+
+# for cmd in CMDS:
+#     output.append(subprocess.run(cmd, shell=True))
+p = subprocess.Popen(args=CMDS[0], shell=True, stdout=subprocess.PIPE)
+# stout = None
+# op = None
+# while stout is not None and op is not None:
+stout, sterr = p.communicate()
+print(stout.decode('utf-8'), end="")
+p.kill()
